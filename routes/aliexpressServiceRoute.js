37773bc8839a759e28108228f3cc7fcef4d9a235
LamTien2003 <ltt209514@gmail.com>
2024-03-16 21:57:32 +0700

    update command start for production

--- routes/aliexpressServiceRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const aliexpressServiceController = require('../controller/aliexpressServiceController');
-
-router.get('/shipping-info', aliexpressServiceController.getShippingInformation);
-
-module.exports = router;
