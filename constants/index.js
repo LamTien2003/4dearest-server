b26e0d1efdcf9b7e3ebba53618505d0a91041f69
LamTien2003 <ltt209514@gmail.com>
2024-04-09 20:38:00 +0700

    change estimate time delivery

--- constants/index.js
+++ constants/index.js
@@ -8,42 +8,42 @@ exports.shippingMethods = [
     {
         country: 'US',
         shippingFee: 3.59,
-        estimateTime: [5, 15],
+        estimateTime: [5, 7],
     },
     {
         country: 'GB',
         shippingFee: 3.59,
-        estimateTime: [7, 20],
+        estimateTime: [7, 15],
     },
     {
         country: 'CA',
         shippingFee: 3.59,
-        estimateTime: [5, 15],
+        estimateTime: [5, 7],
     },
     {
         country: 'AU',
         shippingFee: 3.59,
-        estimateTime: [5, 16],
+        estimateTime: [5, 10],
     },
     {
         country: 'SG',
         shippingFee: 3.59,
-        estimateTime: [5, 16],
+        estimateTime: [5, 10],
     },
     {
         country: 'NZ',
         shippingFee: 3,
-        estimateTime: [10, 20],
+        estimateTime: [10, 15],
     },
     {
         country: 'IT',
         shippingFee: 3,
-        estimateTime: [10, 20],
+        estimateTime: [10, 15],
     },
     {
         country: 'ES',
         shippingFee: 3,
-        estimateTime: [10, 20],
+        estimateTime: [10, 15],
     },
 ];
 
